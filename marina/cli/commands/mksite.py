e33af495a0eb6c9c1253f152d990d0a3a25820fe
Michael Merickel <michael@merickel.org>
2013-07-13 00:45:56 -0600

    dummy mksite impl

--- marina/cli/commands/mksite.py
+++ marina/cli/commands/mksite.py
@@ -5,5 +5,15 @@ class MakeSite(Command):
     def get_description(self):
         return 'Create a new base site configuration.'
 
-    def take_action(self, argv):
-        pass
+    def get_parser(self, prog_name):
+        parser = super(MakeSite, self).get_parser(prog_name)
+        parser.add_argument(
+            'config_file',
+            metavar='DEST_FILE',
+            default='site.yml',
+        )
+        return parser
+
+    def take_action(self, args):
+        with open(args.config_file, 'wb') as fp:
+            fp.write(yaml.dump({}))
