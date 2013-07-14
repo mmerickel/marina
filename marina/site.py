c5f7be188580df6a869e92e82a03e1622e2b5423
whitmo <d.w.morriss@gmail.com>
2013-07-13 18:14:31 -0600

    mild refactor of name input to accept path object

--- marina/site.py
+++ marina/site.py
@@ -1,23 +1,24 @@
-"""
-Generate a site folder and templates
-"""
-from path import path
 from . import defaults
+from path import path
 import yaml
 
 here = path(__file__).parent
 
+
 class SiteConfig(object):
+    """
+    """
     defaults = defaults
 
-    def __init__(self, fp=path('.'), registry=None, dockers=None, services=None):
-        self.name = str(fp.name)
-        self.path = fp
+    def __init__(self, name, base_dir=path('.'), registry=None, dockers=None, services=None):
+        self.path = isinstance(name, path) and name or base_dir / name
+        self.name = isinstance(name, path) and str(name.name) or name
         self.dockers = dockers or [self.defaults.docker_url]
         self.services = services or []
         self.registry = registry
 
     def generate(self):
+        self.path.mkdir_p()
         site_data = dict(dockers=self.dockers,
                          name=self.name,
                          services=self.services,
--- marina/tests/test_make_site.py
+++ marina/tests/test_make_site.py
@@ -10,22 +10,26 @@ class TestSiteConfig(unittest.TestCase):
     def setUp(self):
         self.tmpdir = path(tempfile.mkdtemp())
 
-    def makeone(self):
+    def makeone(self, name):
         from marina.site import SiteConfig
-        return SiteConfig(self.tmpdir)
+        return SiteConfig(self.tmpdir / name)
 
     def test_siteconfig_gen(self):
-        sc = self.makeone()
+        name = 'test1'
+        sc = self.makeone(name)
         sc.generate()
-        assert 'site.yml' in set(x.name for x  in self.tmpdir.files())
-        assert 'nginx' in set(x.name for x  in self.tmpdir.dirs())
-        siteyml = sc.path / 'site.yml'
+
+        site = sc.path 
+        assert 'site.yml' in set(x.name for x  in site.files())
+        assert 'nginx' in set(x.name for x  in site.dirs())
+        siteyml = site / 'site.yml'
+        assert siteyml.parent.exists()
         assert siteyml.text(), siteyml.text()
         with open(siteyml) as stream:
             data = yaml.load(stream)
+
         keys = set(('registry', 'dockers', 'services', 'name'))
         assert set(data.keys()) == keys, keys.difference(set(data.keys()))
-
 
     def tearDown(self):
         self.tmpdir.rmtree_p()
