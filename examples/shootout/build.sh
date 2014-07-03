5a40121ccc906dc9804db1b9ad233183b221cf19
Michael Merickel <michael@merickel.org>
2014-07-03 11:11:46 -0500

    add caching support to examples/shootout

--- examples/shootout/build.sh
+++ examples/shootout/build.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-set -e
+set -eo pipefail
 set -x
 
-apt-get -y -q install build-essential python python-dev python-setuptools python-virtualenv git-core
+apt-get -y -q install python-dev python-setuptools python-virtualenv git-core
 
-cd /srv
-VENV=$(pwd)/venv
-virtualenv $VENV
-git clone https://github.com/Pylons/shootout.git
-cd shootout
-$VENV/bin/python setup.py develop
+APP_ROOT=/app
+
+if [[ -d "$BUILD_CACHE/app" ]]; then
+    cp -a "$BUILD_CACHE/app" "$APP_ROOT"
+    cd "$APP_ROOT"
+    git pull
+else
+    git clone https://github.com/Pylons/shootout.git "$APP_ROOT"
+    cd "$APP_ROOT"
+    virtualenv .
+fi
+
+bin/python setup.py develop
+
+rsync -az "$APP_ROOT/" "$BUILD_CACHE/app"
