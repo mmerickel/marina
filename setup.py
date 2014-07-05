02bf82659230f3c997801623b58ff820f90c39fe
Michael Merickel <michael@digitalartefacts.com>
2014-07-04 23:15:18 -0500

    add some initial testing support

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os
+import sys
+
 from setuptools import find_packages
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,10 +19,32 @@ requires = [
     'subparse',
 ]
 
+tests_require = [
+    'pytest',
+]
+
 entry_points = """
     [console_scripts]
     marina = marina.cli:main
 """
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args or ['tests'])
+        sys.exit(errno)
 
 setup(
     name='marina',
@@ -42,6 +67,10 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=requires,
-    test_suite='marina.tests',
+    tests_require=tests_require,
+    extras_require={
+        'testing': tests_require,
+    },
+    cmdclass={'test': PyTest},
     entry_points=entry_points,
 )
