f751cf8a4a9006234073478fcdc5df4093b25713
Michael Merickel <michael@merickel.org>
2014-11-19 11:33:01 -0600

    prep 0.0.3

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
-Unreleased
-==========
+0.0.3 (2014-11-19)
+==================
 
-- Support docker 1.3.0 and its TLS requirements.
+- Support docker 1.3.x and its TLS requirements.
 
 0.0.2 (2014-07-12)
 ==================
--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ class PyTest(TestCommand):
 
 setup(
     name='marina',
-    version='0.0.2',
+    version='0.0.3',
     description='marina manages docker instances',
     long_description=README + '\n\n' + CHANGES,
     url='https://github.com/mmerickel/marina',
